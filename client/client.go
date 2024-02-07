315ea1cffefdd72db6fab344e48c9dea8b4fd062
Farhandika <18220015@std.stei.itb.ac.id>
2024-02-07 16:31:49 +0700

    added client functionality:

--- client/client.go
+++ client/client.go
@@ -1,35 +1,47 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
+	"os"
 )
 
 func main() {
-	res := groupAnagrams([]string{
-		"sas", "mat", "tam", "mta", "duh", "ill",
-	})
-	fmt.Println(res)
-	//fmt.Println(getAsciiTotal("duh"))
-	//fmt.Println(getAsciiTotal("ill"))
+	conn, err := net.Dial("tcp", "localhost:8081")
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to chatroom server.")
+
+	go readMessages(conn)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		message := scanner.Text()
+		_, err := conn.Write([]byte(message + "\n"))
+		if err != nil {
+			fmt.Println("Error sending message:", err)
+			return
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading standard input:", err)
+		return
+	}
 }
 
-func groupAnagrams(ls []string) [][]string {
-	group := map[[26]byte][]string{}
-	result := make([][]string, 0)
-	for _, s := range ls {
-		key := getKey(s)
-		group[key] = append(group[key], s)
+func readMessages(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading message from server:", err)
+			return
+		}
+		fmt.Print("Received message from server: ", message)
 	}
-	for _, val := range group {
-		result = append(result, val)
-	}
-	return result
-}
-
-func getKey(s string) [26]byte {
-	res := [26]byte{}
-	for _, c := range s {
-		res[c-'a'] += 1
-	}
-	return res
 }
